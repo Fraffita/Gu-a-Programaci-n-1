d60636567e89d8d7b9684b73a421f9e9065cc224
Franco Raffaele <fran.raffa15@gmail.com>
2022-03-30 21:22:44 -0300

    Exercise 1 to 5 finished

--- Assets/scripts/Ejercicio01.cs
+++ Assets/scripts/Ejercicio01.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Ejercicio01 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ //1. Realizá un programa donde se cree una variable entera llamada num1, que inicialmente
+ //valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá el
+ //resultado de multiplicarlo por sí mismo
 
+        void Start()
+    {
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log("num1 vale "+num1);
+        num1 *= num1;
+        Debug.Log("Ahora num1 vale "+num1);
+
+    } 
     // Update is called once per frame
     void Update()
     {
         
     }
 }
+
